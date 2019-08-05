c2ab5edc92efe454fb714f7e83c0986d4dcdfb13
sdesalve <sdesalve@users.noreply.github.com>
2019-08-05 15:41:13 +0200

    Add files via upload

---
+++ config.h
@@ -0,0 +1,46 @@
+///////////////////////////////////////////////////////////////////////////
+//  CONFIGURATION - SOFTWARE
+///////////////////////////////////////////////////////////////////////////
+uint8_t NB_OF_BLE_DISCOVERED_DEVICES = 0;
+BLETrackedDevice BLETrackedDevices[99] = {};
+
+#define BLE_SCANNING_PERIOD   10
+#define MAX_NON_ADV_PERIOD    120000
+
+// Location of the BLE scanner
+#define LOCATION "casa"
+
+// Debug output
+#define DEBUG_SERIAL
+
+// Wi-Fi credentials
+#define WIFI_SSID     "XXXXXXXXXXXXXXX"
+#define WIFI_PASSWORD "XXXXXXXXXXXXXXX"
+
+// Over-the-Air update
+// Not implemented yet
+//#define OTA
+//#define OTA_HOSTNAME  ""    // hostname esp8266-[ChipID] by default
+//#define OTA_PASSWORD  ""    // no password by default
+//#define OTA_PORT      8266  // port 8266 by default
+
+// MQTT
+#define MQTT_USERNAME     "XXXXXXXXXXXXXXX"
+#define MQTT_PASSWORD     "XXXXXXXXXXXXXXX"
+#define MQTT_SERVER       "192.168.1.XXXXX"
+#define MQTT_SERVER_PORT  1883
+
+#define MQTT_CONNECTION_TIMEOUT 5000 // [ms]
+
+// MQTT availability: available/unavailable
+#define MQTT_AVAILABILITY_TOPIC_TEMPLATE  "dss_ble2mqtt/%s/availability"
+// MQTT binary sensor: dss_ble2mqtt/<CHIP_ID>/<LOCATION>/<BLE_ADDRESS>
+#define MQTT_SENSOR_TOPIC_TEMPLATE        "dss_ble2mqtt/%s/%s/%s/state"
+#define MQTT_RSSI_TOPIC_TEMPLATE          "dss_ble2mqtt/%s/%s/%s/rssi"
+
+
+#define MQTT_PAYLOAD_ON   "ON"
+#define MQTT_PAYLOAD_OFF  "OFF"
+
+#define MQTT_PAYLOAD_AVAILABLE    "online"
+#define MQTT_PAYLOAD_UNAVAILABLE  "offline"
